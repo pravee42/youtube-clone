330d2b77440789f26630737c966fd25f5bef55b1
ABIPRAVI <65100038+pravee42@users.noreply.github.com>
2020-11-21 09:07:02 +0530

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const key = "AIzaSyAf-5Kx_4fbYAHGMg8ezSgTitDLYD2eUkw";
+
+export default axios.create({
+  baseURl: "https://www.googleapis.com/youtube/v3";
+  params: {
+    part: 'snippet',
+    maxResults:100,
+    key:key,
+  }
+})
